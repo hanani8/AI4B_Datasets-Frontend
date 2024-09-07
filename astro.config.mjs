4baa77dc449d588ecf9f8399af14c036cb02937b
hanani8 <hananidasilva@gmail.com>
2024-09-07 09:51:55 +0530

    Updated the site variable in astro.config.mjs and rebuilt the files after changing to /lahaja/explore from lahaja/explore

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind()],
+  site: "https://datasets.ai4bharat.org",
   build: {
     assets: "astro",
   },
